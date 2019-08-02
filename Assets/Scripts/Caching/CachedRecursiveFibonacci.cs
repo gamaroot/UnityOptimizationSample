a250d2a9ae84829f335c6cc67593d6d03b4a6a1e
Gama <cgama@gazeus.com>
2019-08-02 11:03:28 -0300

    First commit.

---
+++ Assets/Scripts/Caching/CachedRecursiveFibonacci.cs
@@ -0,0 +1,51 @@
+﻿public class CachedRecursiveFibonacci
+{
+    public static int CalculateFibonacciSum(int index)
+    {
+        int sum = 0;
+        int result = -1;
+        for (int iterationIndex = 0; iterationIndex < index; iterationIndex++)
+        {
+            result = CalculateFibonacciRec(iterationIndex, result);
+            sum += result;
+        }
+
+        return sum;
+    }
+
+    public static int CalculateFibonacci(int index)
+    {
+        if (index == 0)
+        {
+            return 0;
+        }
+        else if (index == 1)
+        {
+            return 1;
+        }
+
+        var b = CalculateFibonacciRec(index - 2,-1);
+        var a =  CalculateFibonacciRec(index-1,b);
+
+        
+        return a+b;
+    }
+
+    private static int CalculateFibonacciRec(int index, int cached = -1)
+    {
+        if (index == 0)
+        {
+            return 0;
+        }
+        else if (index == 1)
+        {
+            return 1;
+        }
+        else
+        {
+            int previous = cached == -1 ? CalculateFibonacci(index - 1) : cached;
+
+            return previous + CalculateFibonacci(index - 2);
+        }
+    }
+}
